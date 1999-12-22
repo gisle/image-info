9e46747dd5d7a05b6d2ad98011da37aa6b490997
Gisle Aas <gisle@aas.no>
1999-12-22 09:40:38 +0000

    Adjustments of fields.

--- lib/Image/Info/PNG.pm
+++ lib/Image/Info/PNG.pm
@@ -26,6 +26,9 @@ sub process_file
     die "Bad PNG signature"
 	unless $signature eq "\x89PNG\x0d\x0a\x1a\x0a";
 
+    $info->push_info(0, "FileMediaType" => "image/png");
+    $info->push_info(0, "FileExt" => "png");
+
     while (1) {
         my($len, $type) = unpack("Na4", my_read($fh, 8));
         $info->push_info(0, "PNG_Chunks", $type) unless $type eq "IDAT";
@@ -36,11 +39,11 @@ sub process_file
 	    my($w, $h, $depth, $ctype, $compression, $filter, $interlace) =
 		unpack("NNCCCCC", $data);
 	    $ctype = {
-		      0 => "GrayScale",
-		      2 => "TrueColor",
-		      3 => "IndexedColor",
-		      4 => "GrayScale/Alpha",
-		      6 => "TrueColor/Alph",
+		      0 => "Gray",
+		      2 => "RGB",
+		      3 => "Indexed",
+		      4 => "GrayA",
+		      6 => "RGBA",
 		     }->{$ctype} || $ctype;
 
 	    $compression = "Deflate" if $compression == 0;
@@ -48,12 +51,13 @@ sub process_file
 	    $interlace = "Adam7" if $interlace == 1;
 
 	    $info->push_info(0, "ImageWidth", $w);
-	    $info->push_info(0, "ImageLength", $h);
+	    $info->push_info(0, "ImageHeight", $h);
 	    $info->push_info(0, "BitDepth", $depth);
 	    $info->push_info(0, "ColorType", $ctype);
 	    $info->push_info(0, "Compression", $compression);
 	    $info->push_info(0, "Filter", $filter);
-	    $info->push_info(0, "Interlace", $interlace);
+	    $info->push_info(0, "Interlace", $interlace)
+		if $interlace;
 	}
 	elsif ($type eq "gAMA" && $len == 4) {
 	    $info->push_info(0, "Gamma", unpack("N", $data)/100_000);
